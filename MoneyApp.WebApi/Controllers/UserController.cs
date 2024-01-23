085745ca491f8af7e400aa8719620f70b01b265f
Vladimir Sinkovets <vovasin49@gmail.com>
2024-01-23 11:31:54 +0300

    remove text method

--- MoneyApp.WebApi/Controllers/UserController.cs
+++ MoneyApp.WebApi/Controllers/UserController.cs
@@ -19,28 +19,7 @@ namespace MoneyApp.WebApi.Controllers
         {
             _mediator = mediator;
         }
-        public static async Task<string> GetRawBodyAsync(
-            HttpRequest request,
-            Encoding encoding = null)
-        {
-            if (!request.Body.CanSeek)
-            {
-                // We only do this if the stream isn't *already* seekable,
-                // as EnableBuffering will create a new stream instance
-                // each time it's called
-                request.EnableBuffering();
-            }
-
-            request.Body.Position = 0;
-
-            var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
-
-            var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-            request.Body.Position = 0;
-
-            return body;
-        }
+        
         [HttpPost]
         [AllowAnonymous]
         [Route("api/register")]
